1089216fe7483ada9efd2d864c763bace5d9feb1
MuraliKrishnan K <k.muralikrishnan@thomsonreuters.com>
2016-09-07 17:35:51 +0530

    firstnetcore

---
+++ FirstNetCoreAng/FirstNetCoreAng/src/FirstNetCoreAng/wwwroot/app/main.js
@@ -0,0 +1,7 @@
+"use strict";
+/// <reference path="../../typings/globals/core-js/index.d.ts" />
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_1 = require('./app.module');
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
